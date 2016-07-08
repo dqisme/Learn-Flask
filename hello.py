cd645d7446cf0fd1b67b1bffd9c891cb02a849e0
dqisme <dqisme@outlook.com>
2016-07-08 14:23:55 +0800

    [dq] #N/A add simple request and db read

--- hello.py
+++ hello.py
@@ -1,6 +1,10 @@
 import requests
+from bson.json_util import dumps
 from flask import Flask, json, request
+from flask_pymongo import PyMongo
 app = Flask(__name__)
+app.config['MONGO_DBNAME'] = 'world'
+mongoDB = PyMongo(app)
 
 
 @app.route('/',methods=['GET', 'POST'])
@@ -15,6 +19,13 @@ def index():
 def forward():
     response = requests.post('http://requestb.in/xp45khxp')
     return json.jsonify(message=response.text)
+
+@app.route('/db',methods=['GET', 'POST'])
+def db():
+    if request.method == 'GET':
+        return json.jsonify(dumps(mongoDB.db.fruits.find()))
+    if request.method == 'POST':
+        return 'todo'
 
 if __name__ == '__main__':
     app.debug = True
